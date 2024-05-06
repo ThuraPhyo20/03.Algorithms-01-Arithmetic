cf8c71577caa8c456d0ddc1ff7f52c0f9398a017
ThuRaPhyo <thuyaphyo7877@gmail.com>
2024-05-06 18:06:24 +0630

    solved

---
+++ arithmetic.js
@@ -0,0 +1,22 @@
+// Fill in the body of each function with the code required
+
+var add = function(num1, num2) {
+   console.log(num1+num2);
+};
+add(10,20);
+
+
+var subtract = function(num1, num2) {
+    console.log(num1 - num2);
+};
+subtract(40,20);
+
+var multiply = function(num1, num2) {
+    console.log(num1*num2);
+};
+multiply(20,6);
+
+var divide = function(num1, num2) {
+    console.log(num1/num2);
+};
+divide(60,2);
